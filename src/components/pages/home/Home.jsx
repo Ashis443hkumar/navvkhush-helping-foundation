dac041b0a34bb3a9f8e27abd15887e1256f25d6b
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-06-09 13:41:47 +0530

    4 files modified

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -5,6 +5,10 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Heading from "../../common/Heading";
 import Image from 'react-bootstrap/Image';
+import Ratio from 'react-bootstrap/Ratio';
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 import "./style.css"
 
@@ -22,7 +26,6 @@ export default function Home() {
         </Row>
        </Container>
       </section>
-
 
      <section className="home_program">
         <Container className="mt-3">
@@ -234,17 +237,141 @@ export default function Home() {
        </Container>
      </section>
 
-     <section className="">
-     <Container className="">
+     <section className="home_stori_Chnage" >
+     <Container className="py-4">
        <Heading title="STORIES OF CHANGE"  />
         <Row>
           <Col xs={12} md={12} lg={12} className="">
             <p>We are happy to introduce some of Lakshyam’s exceptionally brilliant and bright kids who have overcome tremendous adversities and are very close to our heart. We have many such successful warriors who have changed their fate with their sheer hardwork and determination. Their love for life inspires us to extend the best of resources to the</p>
-           
+         
+          </Col>
+           <Col lg={12}>
+             <Carousel slide={true} interval={2000}>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://media.licdn.com/dms/image/D4D22AQFFnkX20jbX_g/feedshare-shrink_2048_1536/0/1685791142446?e=1688601600&v=beta&t=xnH8WAO_d7XCIVDUFYifqFtF75dX4yFLWz50Zl1f6rw"  alt="First slide"  />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://media.licdn.com/dms/image/D4D22AQEwnNriCK-PIw/feedshare-shrink_1280/0/1686051614484?e=1689206400&v=beta&t=acq3cwoloExK0iBeeFuH6HyLfLr4EUuZZl_s6fiaorQ"  alt="Second slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://media.licdn.com/dms/image/D4D22AQHRXtEn5pBZpg/feedshare-shrink_1280/0/1685684249868?e=1688601600&v=beta&t=-7e-oBFp2ubl9jAWutSHfCvIChcY94Ok2ljCYpc-l-k" alt="Third slide" />
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+
+        </Row>
+      </Container>
+     </section>
+
+     <section className="home_child_banner" >
+     <Container className="">
+        <Row style={{display:'flex', textAline:"center" }}>
+          <Col xs={12} md={6} lg={8} className="">
+             <img src="https://www.lakshyam.co.in/wp-content/uploads/2021/12/girls.png" alt="" class="w-100" />
+          </Col>
+          <Col xs={12} md={6} lg={4} className="home_child_content">
+           <p> Contribute for a noble cause. Your help can make a big difference in the life of a child. You can sponsor a child by donating <strong>Rs. 1000 a month</strong> only.</p>
+           <HeroButton title="Donate" />
           </Col>
         </Row>
       </Container>
      </section>
+
+     <section className="home_video_banner" >
+      <Container className="">
+        <Heading title="VIDEOS" />
+        <Row >
+          <Col xs={12} md={4} lg={4}>
+            <Ratio aspectRatio="16x9">
+              <embed type="image/svg+xml" src="https://www.youtube.com/embed/YFSl4MqzdrA?start=39" />
+            </Ratio>
+          </Col>
+          <Col xs={12} md={4} lg={4}>
+            <Ratio aspectRatio="16x9">
+              <embed type="image/svg+xml" src="https://www.youtube.com/embed/YFSl4MqzdrA?start=39" />
+            </Ratio>
+          </Col>
+          <Col xs={12} md={4} lg={4}>
+            <Ratio aspectRatio="16x9">
+              <embed type="image/svg+xml" src="https://www.youtube.com/embed/YFSl4MqzdrA?start=39" />
+            </Ratio>
+          </Col>
+          <p className="text-center my-4">VIEW ALL VIDEOS</p>
+        </Row>
+      </Container>
+     </section>
+
+     <section className="home_card ">
+      <Container className="">
+        <Heading title="SHOP FOR A CAUSE" subtitle="Handmade Products by Women" />
+        <Row >
+        <Col xs={12} md={3} lg={3}>
+        <Card >
+          <Card.Img variant="top" src="https://www.lakshyam.co.in/wp-content/uploads/2021/12/shop-1.png" />
+          <Card.Body>
+            <Card.Title>Doll Pencils</Card.Title>
+            <Card.Text>
+              <p>Some quick example text to build on the Doll Pencils and make up the bulk of the card's content..</p>
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+         <Col xs={12} md={3} lg={3}>
+        <Card >
+          <Card.Img variant="top" src="https://www.lakshyam.co.in/wp-content/uploads/2021/12/shop-2.png" />
+          <Card.Body>
+            <Card.Title>Doll Pencils</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the Doll Pencils and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+
+        <Col xs={12} md={3} lg={3}>
+         <Card >
+          <Card.Img variant="top" src="https://www.lakshyam.co.in/wp-content/uploads/2021/12/shop-3.png" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+
+         <Col xs={12} md={3} lg={3}>
+         <Card >
+          <Card.Img variant="top" src="https://www.lakshyam.co.in/wp-content/uploads/2021/12/shop-4.png" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+        </Row>
+        <p className="text-center my-4">VIEW ALL VIDEOS</p>
+
+      </Container>
+     </section>
+
+
+
 
     </>
   );
